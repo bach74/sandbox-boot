32eeaf48ed1909638d9011290eed39aa717bd251
treichen <treichen@sds.co.at>
2018-01-30 22:42:06 +0100

    inital commit

---
+++ src/main/java/at/sds/demo/HelloWorldController.java
@@ -0,0 +1,26 @@
+package at.sds.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //GET
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    //GET
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World2!");
+    }
+
+    @GetMapping(path ="/hello-world-bean/{id}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String id) {
+        return new HelloWorldBean(String.format("Hello %s", id));
+    }
+}
